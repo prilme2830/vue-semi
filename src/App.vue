3ac507c8e9fb9a422faf3f0dd13f3d5e9f070bba
buangka <aprilmayayta@gmail.com>
2026-08-25 09:55:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+// import FormActivity from "./components/FormActivity.vue";
+
+import StudentForm from "./components/StudentForm.vue";
+
+// import Form1 from "./components/Form1.vue";
+
+// function receiveStudent(studentData) {
+//   // console.log(student.studName);
+//   // console.log(student.course);
+//   // console.log(student.year);
+//   alert(
+//     `Student: ${studentData.studName}\nCourse: ${studentData.course}\nYear: ${studentData.year}`,
+//   );
+//   console.log(studentData);
+// }
+
+
+
+function handleStudent(student) {
+  console.log("Student submitted:", student);
+ 
+}
+</script>
+
+<template>
+  <div>
+    <!-- <FormActivity /> -->
+    <StudentForm @submitted="handleStudent" />
+    
+
+    <!-- <Form1 /> -->
+  </div>
+</template>
---
+++ src/components/StudentDisplay.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  name: String,
+  id: Number,
+  email: String,
+  course: String,
+  yearLevel: String,
+});
+</script>
+<template>
+  <h1>Student Information</h1>
+  <h4>Student Name: {{ name }}</h4>
+  <h4>Student ID: {{ id }}</h4>
+  <h4>Student Email: {{ email }}</h4>
+  <h4>Student Course: {{ course }}</h4>
+  <h4>Student Year Level: {{ yearLevel }}</h4>
+</template>
